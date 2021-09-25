9a284ca92a6b8f64b6314ab442480639274bc26f
Cao Cao <caophuc799@gmail.com>
2021-09-25 15:55:09 +0700

    set config

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -44,8 +44,8 @@ func main() {
 
 	flag.IntVar(&cfg.port, "port", 4000, "Server port to listen on")
 	flag.StringVar(&cfg.env, "env", "development", "Application environment (development|production)")
-	flag.StringVar(&cfg.db.dsn, "dsn", "postgres://postgres:postgres@localhost/go_movies?sslmode=disable", "Postgres connection string")
-	flag.StringVar(&cfg.jwt.secret, "jwt-secret", "test-1234", "secret")
+	cfg.db.dsn = os.Getenv("GO_MOVIES_DB_DSN")
+	cfg.jwt.secret = os.Getenv("GO_MOVIES_JWT")
 	flag.Parse()
 
 	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
